071e5b346a8451975562392c36bc2c3f6e48f3ad
Lonpo <boss@gmail.com>
2023-08-29 09:53:25 +0000

    ayiwa

--- idonko/src/app/app.module.ts
+++ idonko/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { AutresComponent } from './autres/autres.component';
-<<<<<<< HEAD
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IgxProgressBarModule } from 'igniteui-angular';
 import { HammerModule } from '@angular/platform-browser';
@@ -14,7 +13,6 @@ import {
 	IgxCarouselModule,
 	IgxSliderModule
  } from "igniteui-angular";
-=======
 import { FormsModule } from '@angular/forms';
 import{MatButtonModule} from '@angular/material/button';
 import{MatIconModule} from '@angular/material/icon';
@@ -22,7 +20,6 @@ import{MatFormFieldModule} from '@angular/material/form-field';
 import{MatInputModule} from '@angular/material/input';
 import{MatDialogModule} from '@angular/material/dialog';
 import { PopAjoutCompComponent } from './pop-ajout-comp/pop-ajout-comp.component';
->>>>>>> df6ffab39a146f935450c6b08b83b587c376f02e
 
 @NgModule({
   declarations: [
@@ -35,20 +32,17 @@ import { PopAjoutCompComponent } from './pop-ajout-comp/pop-ajout-comp.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     BrowserAnimationsModule,
     IgxProgressBarModule,
     HammerModule,
     IgxCarouselModule,
-    IgxSliderModule
-=======
+    IgxSliderModule,
     FormsModule,
     MatButtonModule,
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
     MatDialogModule,
->>>>>>> df6ffab39a146f935450c6b08b83b587c376f02e
   ],
   providers: [],
   bootstrap: [AppComponent]
